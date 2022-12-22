aeecf17c11abc2d3481fef1075a44c08e6fa0e5c
rasheed <rasheedandrew1@gmail.com>
2022-12-22 12:38:17 -0500

    Initial Commit

---
+++ fox coding interview/fox_coding_interviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  fox_coding_interviewApp.swift
+//  fox coding interview
+//
+//  Created by rasheed andrew on 12/22/22.
+//
+
+import SwiftUI
+
+@main
+struct fox_coding_interviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
